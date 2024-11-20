93ae58f4c3a76999b11cde725ea03fa0f586e566
yu you <471177907@qq.com>
2024-11-20 16:28:09 +0800

    # 初始化

---
+++ EmptySwiftUIProject/EmptySwiftUIProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmptySwiftUIProjectApp.swift
+//  EmptySwiftUIProject
+//
+//  Created by HT-YuYou on 2024/11/20.
+//
+
+import SwiftUI
+
+@main
+struct EmptySwiftUIProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
